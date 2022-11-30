b080596115b3ec3304da48873dff28b640afacb7
sdk-team <sdk-team@alibabacloud.com>
2022-11-30 05:15:42 +0000

    ContinueCreateStack returns DryRunResult when DryRun is set to true.

--- ros-20190910/src/main/java/com/aliyun/sdk/service/ros20190910/models/ContinueCreateStackResponseBody.java
+++ ros-20190910/src/main/java/com/aliyun/sdk/service/ros20190910/models/ContinueCreateStackResponseBody.java
@@ -12,6 +12,9 @@ import com.aliyun.sdk.gateway.pop.models.*;
  * <p>ContinueCreateStackResponseBody</p>
  */
 public class ContinueCreateStackResponseBody extends TeaModel {
+    @NameInMap("DryRunResult")
+    private DryRunResult dryRunResult;
+
     @NameInMap("RequestId")
     private String requestId;
 
@@ -19,6 +22,7 @@ public class ContinueCreateStackResponseBody extends TeaModel {
     private String stackId;
 
     private ContinueCreateStackResponseBody(Builder builder) {
+        this.dryRunResult = builder.dryRunResult;
         this.requestId = builder.requestId;
         this.stackId = builder.stackId;
     }
@@ -29,6 +33,13 @@ public class ContinueCreateStackResponseBody extends TeaModel {
 
     public static ContinueCreateStackResponseBody create() {
         return builder().build();
+    }
+
+    /**
+     * @return dryRunResult
+     */
+    public DryRunResult getDryRunResult() {
+        return this.dryRunResult;
     }
 
     /**
@@ -46,8 +57,17 @@ public class ContinueCreateStackResponseBody extends TeaModel {
     }
 
     public static final class Builder {
+        private DryRunResult dryRunResult; 
         private String requestId; 
         private String stackId; 
+
+        /**
+         * DryRunResult.
+         */
+        public Builder dryRunResult(DryRunResult dryRunResult) {
+            this.dryRunResult = dryRunResult;
+            return this;
+        }
 
         /**
          * RequestId.
@@ -71,4 +91,85 @@ public class ContinueCreateStackResponseBody extends TeaModel {
 
     } 
 
+    public static class DryRunResult extends TeaModel {
+        @NameInMap("ParametersAllowedToBeModified")
+        private java.util.List < String > parametersAllowedToBeModified;
+
+        @NameInMap("ParametersConditionallyAllowedToBeModified")
+        private java.util.List < String > parametersConditionallyAllowedToBeModified;
+
+        @NameInMap("ParametersNotAllowedToBeModified")
+        private java.util.List < String > parametersNotAllowedToBeModified;
+
+        private DryRunResult(Builder builder) {
+            this.parametersAllowedToBeModified = builder.parametersAllowedToBeModified;
+            this.parametersConditionallyAllowedToBeModified = builder.parametersConditionallyAllowedToBeModified;
+            this.parametersNotAllowedToBeModified = builder.parametersNotAllowedToBeModified;
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public static DryRunResult create() {
+            return builder().build();
+        }
+
+        /**
+         * @return parametersAllowedToBeModified
+         */
+        public java.util.List < String > getParametersAllowedToBeModified() {
+            return this.parametersAllowedToBeModified;
+        }
+
+        /**
+         * @return parametersConditionallyAllowedToBeModified
+         */
+        public java.util.List < String > getParametersConditionallyAllowedToBeModified() {
+            return this.parametersConditionallyAllowedToBeModified;
+        }
+
+        /**
+         * @return parametersNotAllowedToBeModified
+         */
+        public java.util.List < String > getParametersNotAllowedToBeModified() {
+            return this.parametersNotAllowedToBeModified;
+        }
+
+        public static final class Builder {
+            private java.util.List < String > parametersAllowedToBeModified; 
+            private java.util.List < String > parametersConditionallyAllowedToBeModified; 
+            private java.util.List < String > parametersNotAllowedToBeModified; 
+
+            /**
+             * ParametersAllowedToBeModified.
+             */
+            public Builder parametersAllowedToBeModified(java.util.List < String > parametersAllowedToBeModified) {
+                this.parametersAllowedToBeModified = parametersAllowedToBeModified;
+                return this;
+            }
+
+            /**
+             * ParametersConditionallyAllowedToBeModified.
+             */
+            public Builder parametersConditionallyAllowedToBeModified(java.util.List < String > parametersConditionallyAllowedToBeModified) {
+                this.parametersConditionallyAllowedToBeModified = parametersConditionallyAllowedToBeModified;
+                return this;
+            }
+
+            /**
+             * ParametersNotAllowedToBeModified.
+             */
+            public Builder parametersNotAllowedToBeModified(java.util.List < String > parametersNotAllowedToBeModified) {
+                this.parametersNotAllowedToBeModified = parametersNotAllowedToBeModified;
+                return this;
+            }
+
+            public DryRunResult build() {
+                return new DryRunResult(this);
+            } 
+
+        } 
+
+    }
 }
