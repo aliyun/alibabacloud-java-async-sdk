9ba6c021d4001662d300d4ade58ff971ff5608f3
sdk-team <sdk-team@alibabacloud.com>
2023-06-13 06:18:09 +0000

    -stream text

--- avatar-20220130/src/main/java/com/aliyun/sdk/service/avatar20220130/models/SendMessageRequest.java
+++ avatar-20220130/src/main/java/com/aliyun/sdk/service/avatar20220130/models/SendMessageRequest.java
@@ -21,6 +21,10 @@ public class SendMessageRequest extends Request {
     private String sessionId;
 
     @Query
+    @NameInMap("StreamExtension")
+    private StreamExtension streamExtension;
+
+    @Query
     @NameInMap("TenantId")
     private Long tenantId;
 
@@ -36,6 +40,7 @@ public class SendMessageRequest extends Request {
         super(builder);
         this.feedback = builder.feedback;
         this.sessionId = builder.sessionId;
+        this.streamExtension = builder.streamExtension;
         this.tenantId = builder.tenantId;
         this.textRequest = builder.textRequest;
         this.VAMLRequest = builder.VAMLRequest;
@@ -69,6 +74,13 @@ public class SendMessageRequest extends Request {
     }
 
     /**
+     * @return streamExtension
+     */
+    public StreamExtension getStreamExtension() {
+        return this.streamExtension;
+    }
+
+    /**
      * @return tenantId
      */
     public Long getTenantId() {
@@ -92,6 +104,7 @@ public class SendMessageRequest extends Request {
     public static final class Builder extends Request.Builder<SendMessageRequest, Builder> {
         private Boolean feedback; 
         private String sessionId; 
+        private StreamExtension streamExtension; 
         private Long tenantId; 
         private TextRequest textRequest; 
         private VAMLRequest VAMLRequest; 
@@ -104,6 +117,7 @@ public class SendMessageRequest extends Request {
             super(request);
             this.feedback = request.feedback;
             this.sessionId = request.sessionId;
+            this.streamExtension = request.streamExtension;
             this.tenantId = request.tenantId;
             this.textRequest = request.textRequest;
             this.VAMLRequest = request.VAMLRequest;
@@ -124,6 +138,16 @@ public class SendMessageRequest extends Request {
         public Builder sessionId(String sessionId) {
             this.putQueryParameter("SessionId", sessionId);
             this.sessionId = sessionId;
+            return this;
+        }
+
+        /**
+         * StreamExtension.
+         */
+        public Builder streamExtension(StreamExtension streamExtension) {
+            String streamExtensionShrink = shrink(streamExtension, "StreamExtension", "json");
+            this.putQueryParameter("StreamExtension", streamExtensionShrink);
+            this.streamExtension = streamExtension;
             return this;
         }
 
@@ -163,6 +187,87 @@ public class SendMessageRequest extends Request {
 
     } 
 
+    public static class StreamExtension extends TeaModel {
+        @NameInMap("Index")
+        private Integer index;
+
+        @NameInMap("IsStream")
+        private Boolean isStream;
+
+        @NameInMap("Position")
+        private String position;
+
+        private StreamExtension(Builder builder) {
+            this.index = builder.index;
+            this.isStream = builder.isStream;
+            this.position = builder.position;
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public static StreamExtension create() {
+            return builder().build();
+        }
+
+        /**
+         * @return index
+         */
+        public Integer getIndex() {
+            return this.index;
+        }
+
+        /**
+         * @return isStream
+         */
+        public Boolean getIsStream() {
+            return this.isStream;
+        }
+
+        /**
+         * @return position
+         */
+        public String getPosition() {
+            return this.position;
+        }
+
+        public static final class Builder {
+            private Integer index; 
+            private Boolean isStream; 
+            private String position; 
+
+            /**
+             * Index.
+             */
+            public Builder index(Integer index) {
+                this.index = index;
+                return this;
+            }
+
+            /**
+             * IsStream.
+             */
+            public Builder isStream(Boolean isStream) {
+                this.isStream = isStream;
+                return this;
+            }
+
+            /**
+             * Position.
+             */
+            public Builder position(String position) {
+                this.position = position;
+                return this;
+            }
+
+            public StreamExtension build() {
+                return new StreamExtension(this);
+            } 
+
+        } 
+
+    }
     public static class TextRequest extends TeaModel {
         @NameInMap("CommandType")
         private String commandType;
