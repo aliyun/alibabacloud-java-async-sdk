50d0f1d372c3af18a7be8175167edf29e54babef
sdk-team <sdk-team@alibabacloud.com>
2023-09-12 11:18:53 +0000

    Generated java-async 2022-07-29 for docmind-api.

--- docmind-api-20220729/src/main/java/com/aliyun/sdk/service/docmind_api20220729/models/GetSingleDocumentExtractResultResponse.java
+++ docmind-api-20220729/src/main/java/com/aliyun/sdk/service/docmind_api20220729/models/GetSingleDocumentExtractResultResponse.java
@@ -16,6 +16,10 @@ public class GetSingleDocumentExtractResultResponse extends Response {
     @Validation(required = true)
     private java.util.Map < String, String > headers;
 
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    private Integer statusCode;
+
     @NameInMap("body")
     @Validation(required = true)
     private GetSingleDocumentExtractResultResponseBody body;
@@ -23,6 +27,7 @@ public class GetSingleDocumentExtractResultResponse extends Response {
     private GetSingleDocumentExtractResultResponse(BuilderImpl builder) {
         super(builder);
         this.headers = builder.headers;
+        this.statusCode = builder.statusCode;
         this.body = builder.body;
     }
 
@@ -43,6 +48,13 @@ public class GetSingleDocumentExtractResultResponse extends Response {
     }
 
     /**
+     * @return statusCode
+     */
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    /**
      * @return body
      */
     public GetSingleDocumentExtractResultResponseBody getBody() {
@@ -52,6 +64,8 @@ public class GetSingleDocumentExtractResultResponse extends Response {
     public interface Builder extends Response.Builder<GetSingleDocumentExtractResultResponse, Builder> {
 
         Builder headers(java.util.Map < String, String > headers);
+
+        Builder statusCode(Integer statusCode);
 
         Builder body(GetSingleDocumentExtractResultResponseBody body);
 
@@ -64,6 +78,7 @@ public class GetSingleDocumentExtractResultResponse extends Response {
             extends Response.BuilderImpl<GetSingleDocumentExtractResultResponse, Builder>
             implements Builder {
         private java.util.Map < String, String > headers; 
+        private Integer statusCode; 
         private GetSingleDocumentExtractResultResponseBody body; 
 
         private BuilderImpl() {
@@ -73,6 +88,7 @@ public class GetSingleDocumentExtractResultResponse extends Response {
         private BuilderImpl(GetSingleDocumentExtractResultResponse response) {
             super(response);
             this.headers = response.headers;
+            this.statusCode = response.statusCode;
             this.body = response.body;
         } 
 
@@ -82,6 +98,15 @@ public class GetSingleDocumentExtractResultResponse extends Response {
         @Override
         public Builder headers(java.util.Map < String, String > headers) {
             this.headers = headers;
+            return this;
+        }
+
+        /**
+         * statusCode.
+         */
+        @Override
+        public Builder statusCode(Integer statusCode) {
+            this.statusCode = statusCode;
             return this;
         }
 
--- docmind-api-20220729/src/main/java/com/aliyun/sdk/service/docmind_api20220729/models/RetryTradeDocumentExtractResponse.java
+++ docmind-api-20220729/src/main/java/com/aliyun/sdk/service/docmind_api20220729/models/RetryTradeDocumentExtractResponse.java
@@ -16,6 +16,10 @@ public class RetryTradeDocumentExtractResponse extends Response {
     @Validation(required = true)
     private java.util.Map < String, String > headers;
 
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    private Integer statusCode;
+
     @NameInMap("body")
     @Validation(required = true)
     private RetryTradeDocumentExtractResponseBody body;
@@ -23,6 +27,7 @@ public class RetryTradeDocumentExtractResponse extends Response {
     private RetryTradeDocumentExtractResponse(BuilderImpl builder) {
         super(builder);
         this.headers = builder.headers;
+        this.statusCode = builder.statusCode;
         this.body = builder.body;
     }
 
@@ -43,6 +48,13 @@ public class RetryTradeDocumentExtractResponse extends Response {
     }
 
     /**
+     * @return statusCode
+     */
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    /**
      * @return body
      */
     public RetryTradeDocumentExtractResponseBody getBody() {
@@ -52,6 +64,8 @@ public class RetryTradeDocumentExtractResponse extends Response {
     public interface Builder extends Response.Builder<RetryTradeDocumentExtractResponse, Builder> {
 
         Builder headers(java.util.Map < String, String > headers);
+
+        Builder statusCode(Integer statusCode);
 
         Builder body(RetryTradeDocumentExtractResponseBody body);
 
@@ -64,6 +78,7 @@ public class RetryTradeDocumentExtractResponse extends Response {
             extends Response.BuilderImpl<RetryTradeDocumentExtractResponse, Builder>
             implements Builder {
         private java.util.Map < String, String > headers; 
+        private Integer statusCode; 
         private RetryTradeDocumentExtractResponseBody body; 
 
         private BuilderImpl() {
@@ -73,6 +88,7 @@ public class RetryTradeDocumentExtractResponse extends Response {
         private BuilderImpl(RetryTradeDocumentExtractResponse response) {
             super(response);
             this.headers = response.headers;
+            this.statusCode = response.statusCode;
             this.body = response.body;
         } 
 
@@ -82,6 +98,15 @@ public class RetryTradeDocumentExtractResponse extends Response {
         @Override
         public Builder headers(java.util.Map < String, String > headers) {
             this.headers = headers;
+            return this;
+        }
+
+        /**
+         * statusCode.
+         */
+        @Override
+        public Builder statusCode(Integer statusCode) {
+            this.statusCode = statusCode;
             return this;
         }
 
