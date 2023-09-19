1008cb4b4fa19c8c55ba5b6a5e768cfd14f50876
sdk-team <sdk-team@alibabacloud.com>
2023-09-19 11:00:00 +0000

    Generated java-async 2018-12-03 for Sas.

--- sas-20181203/src/main/java/com/aliyun/sdk/service/sas20181203/models/ListInterceptionHistoryRequest.java
+++ sas-20181203/src/main/java/com/aliyun/sdk/service/sas20181203/models/ListInterceptionHistoryRequest.java
@@ -197,12 +197,11 @@ public class ListInterceptionHistoryRequest extends Request {
         }
 
         /**
-         * 设置请求和接收消息的语言类型。默认值**zh**。取值：
+         * The language of the content within the request and response. Default value: **zh**. Valid values:
          * <p>
          * 
-         * 
-         * - **zh**：中文
-         * - **en**：英文
+         * *   **zh**: Chinese
+         * *   **en**: English
          */
         public Builder lang(String lang) {
             this.putQueryParameter("Lang", lang);
