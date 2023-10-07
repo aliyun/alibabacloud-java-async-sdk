ba01b06d70a6a88c58ec0db39a4a682960fe27f1
sdk-team <sdk-team@alibabacloud.com>
2023-10-07 17:08:32 +0000

    Generated java-async 2021-02-04 for AIWorkSpace.

--- aiworkspace-20210204/src/main/java/com/aliyun/sdk/service/aiworkspace20210204/models/DeleteMembersResponseBody.java
+++ aiworkspace-20210204/src/main/java/com/aliyun/sdk/service/aiworkspace20210204/models/DeleteMembersResponseBody.java
@@ -12,10 +12,18 @@ import com.aliyun.sdk.gateway.pop.models.*;
  * <p>DeleteMembersResponseBody</p>
  */
 public class DeleteMembersResponseBody extends TeaModel {
+    @NameInMap("Code")
+    private String code;
+
+    @NameInMap("Message")
+    private String message;
+
     @NameInMap("RequestId")
     private String requestId;
 
     private DeleteMembersResponseBody(Builder builder) {
+        this.code = builder.code;
+        this.message = builder.message;
         this.requestId = builder.requestId;
     }
 
@@ -28,6 +36,20 @@ public class DeleteMembersResponseBody extends TeaModel {
     }
 
     /**
+     * @return code
+     */
+    public String getCode() {
+        return this.code;
+    }
+
+    /**
+     * @return message
+     */
+    public String getMessage() {
+        return this.message;
+    }
+
+    /**
      * @return requestId
      */
     public String getRequestId() {
@@ -35,7 +57,25 @@ public class DeleteMembersResponseBody extends TeaModel {
     }
 
     public static final class Builder {
+        private String code; 
+        private String message; 
         private String requestId; 
+
+        /**
+         * Code.
+         */
+        public Builder code(String code) {
+            this.code = code;
+            return this;
+        }
+
+        /**
+         * Message.
+         */
+        public Builder message(String message) {
+            this.message = message;
+            return this;
+        }
 
         /**
          * RequestId.
