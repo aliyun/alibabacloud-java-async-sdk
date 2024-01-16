453407713899ad3ee9b01e2f2fdc79f9b91bb346
sdk-team <sdk-team@alibabacloud.com>
2024-01-16 10:31:05 +0000

    Generated java-async 2023-08-01 for AiMiaoBi.

---
+++ aimiaobi-20230801/src/main/java/com/aliyun/sdk/service/aimiaobi20230801/AsyncClient.java
@@ -0,0 +1,23 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.sdk.service.aimiaobi20230801;
+
+import com.aliyun.core.utils.SdkAutoCloseable;
+import com.aliyun.sdk.service.aimiaobi20230801.models.*;
+import darabonba.core.async.*;
+import darabonba.core.sync.*;
+
+import java.util.concurrent.CompletableFuture;
+
+public interface AsyncClient extends SdkAutoCloseable {
+
+    static DefaultAsyncClientBuilder builder() {
+        return new DefaultAsyncClientBuilder();
+    }
+
+    static AsyncClient create() {
+        return builder().build();
+    }
+
+    CompletableFuture<CreateTokenResponse> createToken(CreateTokenRequest request);
+
+}
---
+++ aimiaobi-20230801/src/main/java/com/aliyun/sdk/service/aimiaobi20230801/models/CreateTokenResponseBody.java
@@ -0,0 +1,215 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.sdk.service.aimiaobi20230801.models;
+
+import com.aliyun.core.annotation.*;
+import darabonba.core.RequestModel;
+import darabonba.core.TeaModel;
+import com.aliyun.sdk.gateway.pop.models.*;
+
+/**
+ * {@link CreateTokenResponseBody} extends {@link TeaModel}
+ *
+ * <p>CreateTokenResponseBody</p>
+ */
+public class CreateTokenResponseBody extends TeaModel {
+    @NameInMap("Code")
+    private String code;
+
+    @NameInMap("Data")
+    private Data data;
+
+    @NameInMap("HttpStatusCode")
+    private String httpStatusCode;
+
+    @NameInMap("Message")
+    private String message;
+
+    @NameInMap("RequestId")
+    private String requestId;
+
+    @NameInMap("Success")
+    private Boolean success;
+
+    private CreateTokenResponseBody(Builder builder) {
+        this.code = builder.code;
+        this.data = builder.data;
+        this.httpStatusCode = builder.httpStatusCode;
+        this.message = builder.message;
+        this.requestId = builder.requestId;
+        this.success = builder.success;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static CreateTokenResponseBody create() {
+        return builder().build();
+    }
+
+    /**
+     * @return code
+     */
+    public String getCode() {
+        return this.code;
+    }
+
+    /**
+     * @return data
+     */
+    public Data getData() {
+        return this.data;
+    }
+
+    /**
+     * @return httpStatusCode
+     */
+    public String getHttpStatusCode() {
+        return this.httpStatusCode;
+    }
+
+    /**
+     * @return message
+     */
+    public String getMessage() {
+        return this.message;
+    }
+
+    /**
+     * @return requestId
+     */
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+    /**
+     * @return success
+     */
+    public Boolean getSuccess() {
+        return this.success;
+    }
+
+    public static final class Builder {
+        private String code; 
+        private Data data; 
+        private String httpStatusCode; 
+        private String message; 
+        private String requestId; 
+        private Boolean success; 
+
+        /**
+         * Code.
+         */
+        public Builder code(String code) {
+            this.code = code;
+            return this;
+        }
+
+        /**
+         * Data.
+         */
+        public Builder data(Data data) {
+            this.data = data;
+            return this;
+        }
+
+        /**
+         * HttpStatusCode.
+         */
+        public Builder httpStatusCode(String httpStatusCode) {
+            this.httpStatusCode = httpStatusCode;
+            return this;
+        }
+
+        /**
+         * Message.
+         */
+        public Builder message(String message) {
+            this.message = message;
+            return this;
+        }
+
+        /**
+         * RequestId.
+         */
+        public Builder requestId(String requestId) {
+            this.requestId = requestId;
+            return this;
+        }
+
+        /**
+         * Success.
+         */
+        public Builder success(Boolean success) {
+            this.success = success;
+            return this;
+        }
+
+        public CreateTokenResponseBody build() {
+            return new CreateTokenResponseBody(this);
+        } 
+
+    } 
+
+    public static class Data extends TeaModel {
+        @NameInMap("ExpiredTime")
+        private Long expiredTime;
+
+        @NameInMap("Token")
+        private String token;
+
+        private Data(Builder builder) {
+            this.expiredTime = builder.expiredTime;
+            this.token = builder.token;
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public static Data create() {
+            return builder().build();
+        }
+
+        /**
+         * @return expiredTime
+         */
+        public Long getExpiredTime() {
+            return this.expiredTime;
+        }
+
+        /**
+         * @return token
+         */
+        public String getToken() {
+            return this.token;
+        }
+
+        public static final class Builder {
+            private Long expiredTime; 
+            private String token; 
+
+            /**
+             * ExpiredTime.
+             */
+            public Builder expiredTime(Long expiredTime) {
+                this.expiredTime = expiredTime;
+                return this;
+            }
+
+            /**
+             * Token.
+             */
+            public Builder token(String token) {
+                this.token = token;
+                return this;
+            }
+
+            public Data build() {
+                return new Data(this);
+            } 
+
+        } 
+
+    }
+}
