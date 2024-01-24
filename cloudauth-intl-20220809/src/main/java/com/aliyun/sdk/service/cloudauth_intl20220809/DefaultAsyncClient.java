3a9753da3d430eaf369351e4d7516446b0ea9783
sdk-team <sdk-team@alibabacloud.com>
2024-01-24 17:13:54 +0000

    Generated java-async 2022-08-09 for Cloudauth-intl.

--- cloudauth-intl-20220809/src/main/java/com/aliyun/sdk/service/cloudauth_intl20220809/DefaultAsyncClient.java
+++ cloudauth-intl-20220809/src/main/java/com/aliyun/sdk/service/cloudauth_intl20220809/DefaultAsyncClient.java
@@ -47,7 +47,7 @@ public final class DefaultAsyncClient implements AsyncClient {
     public CompletableFuture<CardOcrResponse> cardOcr(CardOcrRequest request) {
         try {
             this.handler.validateRequestModel(request);
-            TeaRequest teaRequest = REQUEST.copy().setStyle(RequestStyle.RPC).setAction("CardOcr").setMethod(HttpMethod.POST).setPathRegex("/").setBodyType(BodyType.JSON).setBodyIsForm(false).setReqBodyType(BodyType.JSON).formModel(request);
+            TeaRequest teaRequest = REQUEST.copy().setStyle(RequestStyle.RPC).setAction("CardOcr").setMethod(HttpMethod.POST).setPathRegex("/").setBodyType(BodyType.JSON).setBodyIsForm(true).setReqBodyType(BodyType.FORM).formModel(request);
             ClientExecutionParams params = new ClientExecutionParams().withInput(request).withRequest(teaRequest).withOutput(CardOcrResponse.create());
             return this.handler.execute(params);
         } catch (Exception e) {
