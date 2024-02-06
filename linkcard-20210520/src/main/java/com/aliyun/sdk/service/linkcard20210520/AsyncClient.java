e1baf74923deb0e4742e842b9812df7a25ce1f64
sdk-team <sdk-team@alibabacloud.com>
2024-02-06 17:12:12 +0000

    Generated java-async 2021-05-20 for Linkcard.

--- linkcard-20210520/src/main/java/com/aliyun/sdk/service/linkcard20210520/AsyncClient.java
+++ linkcard-20210520/src/main/java/com/aliyun/sdk/service/linkcard20210520/AsyncClient.java
@@ -3,6 +3,7 @@ package com.aliyun.sdk.service.linkcard20210520;
 
 import com.aliyun.core.utils.SdkAutoCloseable;
 import com.aliyun.sdk.service.linkcard20210520.models.*;
+import darabonba.core.*;
 import darabonba.core.async.*;
 import darabonba.core.sync.*;
 
--- linkcard-20210520/src/main/java/com/aliyun/sdk/service/linkcard20210520/models/GetCardLatestFlowResponse.java
+++ linkcard-20210520/src/main/java/com/aliyun/sdk/service/linkcard20210520/models/GetCardLatestFlowResponse.java
@@ -13,15 +13,12 @@ import com.aliyun.sdk.gateway.pop.models.*;
  */
 public class GetCardLatestFlowResponse extends Response {
     @NameInMap("headers")
-    @Validation(required = true)
     private java.util.Map < String, String > headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     private Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     private GetCardLatestFlowResponseBody body;
 
     private GetCardLatestFlowResponse(BuilderImpl builder) {
