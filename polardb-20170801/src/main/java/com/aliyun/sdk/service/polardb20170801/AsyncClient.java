4caa5f68e9980c17110b297105fcf4c39dcb7b12
sdk-team <sdk-team@alibabacloud.com>
2024-02-21 09:42:45 +0000

    Generated java-async 2017-08-01 for polardb.

--- polardb-20170801/src/main/java/com/aliyun/sdk/service/polardb20170801/AsyncClient.java
+++ polardb-20170801/src/main/java/com/aliyun/sdk/service/polardb20170801/AsyncClient.java
@@ -3,6 +3,7 @@ package com.aliyun.sdk.service.polardb20170801;
 
 import com.aliyun.core.utils.SdkAutoCloseable;
 import com.aliyun.sdk.service.polardb20170801.models.*;
+import darabonba.core.*;
 import darabonba.core.async.*;
 import darabonba.core.sync.*;
 
