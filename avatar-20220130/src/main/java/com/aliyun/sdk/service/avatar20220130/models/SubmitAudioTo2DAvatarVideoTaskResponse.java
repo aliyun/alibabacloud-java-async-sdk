29e8acf52689387d5a7c94027441be1ef4b0c580
sdk-team <sdk-team@alibabacloud.com>
2024-02-27 03:14:04 +0000

    Generated java-async 2022-01-30 for avatar.

--- avatar-20220130/src/main/java/com/aliyun/sdk/service/avatar20220130/models/SubmitAudioTo2DAvatarVideoTaskResponse.java
+++ avatar-20220130/src/main/java/com/aliyun/sdk/service/avatar20220130/models/SubmitAudioTo2DAvatarVideoTaskResponse.java
@@ -13,15 +13,12 @@ import com.aliyun.sdk.gateway.pop.models.*;
  */
 public class SubmitAudioTo2DAvatarVideoTaskResponse extends Response {
     @NameInMap("headers")
-    @Validation(required = true)
     private java.util.Map < String, String > headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     private Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     private SubmitAudioTo2DAvatarVideoTaskResponseBody body;
 
     private SubmitAudioTo2DAvatarVideoTaskResponse(BuilderImpl builder) {
