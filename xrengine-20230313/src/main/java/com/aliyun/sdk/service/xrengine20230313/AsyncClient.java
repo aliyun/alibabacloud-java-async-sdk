16502a195ffb3d9a1a9ea2958536185c894d6610
sdk-team <sdk-team@alibabacloud.com>
2024-02-29 07:01:50 +0000

    Generated java-async 2023-03-13 for xrEngine.

--- xrengine-20230313/src/main/java/com/aliyun/sdk/service/xrengine20230313/AsyncClient.java
+++ xrengine-20230313/src/main/java/com/aliyun/sdk/service/xrengine20230313/AsyncClient.java
@@ -3,6 +3,7 @@ package com.aliyun.sdk.service.xrengine20230313;
 
 import com.aliyun.core.utils.SdkAutoCloseable;
 import com.aliyun.sdk.service.xrengine20230313.models.*;
+import darabonba.core.*;
 import darabonba.core.async.*;
 import darabonba.core.sync.*;
 
