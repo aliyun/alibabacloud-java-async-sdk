48cfc5487cc13b8e9c42f7ca96d730f0b1dc0fcf
sdk-team <sdk-team@alibabacloud.com>
2024-03-05 17:09:47 +0000

    Generated java-async 2023-07-11 for CarbonFootprint.

--- carbonfootprint-20230711/src/main/java/com/aliyun/sdk/service/carbonfootprint20230711/AsyncClient.java
+++ carbonfootprint-20230711/src/main/java/com/aliyun/sdk/service/carbonfootprint20230711/AsyncClient.java
@@ -3,6 +3,7 @@ package com.aliyun.sdk.service.carbonfootprint20230711;
 
 import com.aliyun.core.utils.SdkAutoCloseable;
 import com.aliyun.sdk.service.carbonfootprint20230711.models.*;
+import darabonba.core.*;
 import darabonba.core.async.*;
 import darabonba.core.sync.*;
 
--- carbonfootprint-20230711/src/main/java/com/aliyun/sdk/service/carbonfootprint20230711/models/VerifyResponseBody.java
+++ carbonfootprint-20230711/src/main/java/com/aliyun/sdk/service/carbonfootprint20230711/models/VerifyResponseBody.java
@@ -71,12 +71,93 @@ public class VerifyResponseBody extends TeaModel {
 
     } 
 
+    public static class AllMultiAccountUids extends TeaModel {
+        @NameInMap("accountId")
+        private String accountId;
+
+        @NameInMap("displayName")
+        private String displayName;
+
+        private AllMultiAccountUids(Builder builder) {
+            this.accountId = builder.accountId;
+            this.displayName = builder.displayName;
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public static AllMultiAccountUids create() {
+            return builder().build();
+        }
+
+        /**
+         * @return accountId
+         */
+        public String getAccountId() {
+            return this.accountId;
+        }
+
+        /**
+         * @return displayName
+         */
+        public String getDisplayName() {
+            return this.displayName;
+        }
+
+        public static final class Builder {
+            private String accountId; 
+            private String displayName; 
+
+            /**
+             * accountId.
+             */
+            public Builder accountId(String accountId) {
+                this.accountId = accountId;
+                return this;
+            }
+
+            /**
+             * displayName.
+             */
+            public Builder displayName(String displayName) {
+                this.displayName = displayName;
+                return this;
+            }
+
+            public AllMultiAccountUids build() {
+                return new AllMultiAccountUids(this);
+            } 
+
+        } 
+
+    }
     public static class Data extends TeaModel {
         @NameInMap("AllowedUids")
         private java.util.List < String > allowedUids;
 
+        @NameInMap("accountType")
+        private Integer accountType;
+
+        @NameInMap("allMultiAccountUids")
+        private java.util.List < AllMultiAccountUids> allMultiAccountUids;
+
+        @NameInMap("code")
+        private String code;
+
+        @NameInMap("message")
+        private String message;
+
+        @NameInMap("multiAccountsAllow")
+        private Integer multiAccountsAllow;
+
         private Data(Builder builder) {
             this.allowedUids = builder.allowedUids;
+            this.accountType = builder.accountType;
+            this.allMultiAccountUids = builder.allMultiAccountUids;
+            this.code = builder.code;
+            this.message = builder.message;
+            this.multiAccountsAllow = builder.multiAccountsAllow;
         }
 
         public static Builder builder() {
@@ -94,14 +175,94 @@ public class VerifyResponseBody extends TeaModel {
             return this.allowedUids;
         }
 
+        /**
+         * @return accountType
+         */
+        public Integer getAccountType() {
+            return this.accountType;
+        }
+
+        /**
+         * @return allMultiAccountUids
+         */
+        public java.util.List < AllMultiAccountUids> getAllMultiAccountUids() {
+            return this.allMultiAccountUids;
+        }
+
+        /**
+         * @return code
+         */
+        public String getCode() {
+            return this.code;
+        }
+
+        /**
+         * @return message
+         */
+        public String getMessage() {
+            return this.message;
+        }
+
+        /**
+         * @return multiAccountsAllow
+         */
+        public Integer getMultiAccountsAllow() {
+            return this.multiAccountsAllow;
+        }
+
         public static final class Builder {
             private java.util.List < String > allowedUids; 
+            private Integer accountType; 
+            private java.util.List < AllMultiAccountUids> allMultiAccountUids; 
+            private String code; 
+            private String message; 
+            private Integer multiAccountsAllow; 
 
             /**
              * AllowedUids.
              */
             public Builder allowedUids(java.util.List < String > allowedUids) {
                 this.allowedUids = allowedUids;
+                return this;
+            }
+
+            /**
+             * accountType.
+             */
+            public Builder accountType(Integer accountType) {
+                this.accountType = accountType;
+                return this;
+            }
+
+            /**
+             * allMultiAccountUids.
+             */
+            public Builder allMultiAccountUids(java.util.List < AllMultiAccountUids> allMultiAccountUids) {
+                this.allMultiAccountUids = allMultiAccountUids;
+                return this;
+            }
+
+            /**
+             * code.
+             */
+            public Builder code(String code) {
+                this.code = code;
+                return this;
+            }
+
+            /**
+             * message.
+             */
+            public Builder message(String message) {
+                this.message = message;
+                return this;
+            }
+
+            /**
+             * multiAccountsAllow.
+             */
+            public Builder multiAccountsAllow(Integer multiAccountsAllow) {
+                this.multiAccountsAllow = multiAccountsAllow;
                 return this;
             }
 
