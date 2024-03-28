c26d164c085fd8c232a63be8c7db7bc1c02d7923
sdk-team <sdk-team@alibabacloud.com>
2024-03-28 17:11:55 +0000

    Generated java-async 2020-03-06 for companyreg.

--- companyreg-20200306/src/main/java/com/aliyun/sdk/service/companyreg20200306/models/QueryInstanceResponse.java
+++ companyreg-20200306/src/main/java/com/aliyun/sdk/service/companyreg20200306/models/QueryInstanceResponse.java
@@ -13,15 +13,12 @@ import com.aliyun.sdk.gateway.pop.models.*;
  */
 public class QueryInstanceResponse extends Response {
     @NameInMap("headers")
-    @Validation(required = true)
     private java.util.Map < String, String > headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     private Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     private QueryInstanceResponseBody body;
 
     private QueryInstanceResponse(BuilderImpl builder) {
--- companyreg-20200306/src/main/java/com/aliyun/sdk/service/companyreg20200306/models/RejectUserSolutionResponse.java
+++ companyreg-20200306/src/main/java/com/aliyun/sdk/service/companyreg20200306/models/RejectUserSolutionResponse.java
@@ -13,15 +13,12 @@ import com.aliyun.sdk.gateway.pop.models.*;
  */
 public class RejectUserSolutionResponse extends Response {
     @NameInMap("headers")
-    @Validation(required = true)
     private java.util.Map < String, String > headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     private Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     private RejectUserSolutionResponseBody body;
 
     private RejectUserSolutionResponse(BuilderImpl builder) {
