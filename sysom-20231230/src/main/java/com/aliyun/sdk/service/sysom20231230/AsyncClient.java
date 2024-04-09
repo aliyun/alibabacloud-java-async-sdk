373526354776f8d07446b7dee9e016e1f4eb6efb
sdk-team <sdk-team@alibabacloud.com>
2024-04-09 07:27:59 +0000

    Generated java-async 2023-12-30 for SysOM.

--- sysom-20231230/src/main/java/com/aliyun/sdk/service/sysom20231230/AsyncClient.java
+++ sysom-20231230/src/main/java/com/aliyun/sdk/service/sysom20231230/AsyncClient.java
@@ -3,6 +3,7 @@ package com.aliyun.sdk.service.sysom20231230;
 
 import com.aliyun.core.utils.SdkAutoCloseable;
 import com.aliyun.sdk.service.sysom20231230.models.*;
+import darabonba.core.*;
 import darabonba.core.async.*;
 import darabonba.core.sync.*;
 
