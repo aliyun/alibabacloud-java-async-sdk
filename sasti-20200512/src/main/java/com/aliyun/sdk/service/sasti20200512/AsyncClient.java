ac3624c888e107e2797abf664e198aed317f23b0
sdk-team <sdk-team@alibabacloud.com>
2024-04-16 17:08:17 +0000

    Generated java-async 2020-05-12 for Sasti.

--- sasti-20200512/src/main/java/com/aliyun/sdk/service/sasti20200512/AsyncClient.java
+++ sasti-20200512/src/main/java/com/aliyun/sdk/service/sasti20200512/AsyncClient.java
@@ -3,6 +3,7 @@ package com.aliyun.sdk.service.sasti20200512;
 
 import com.aliyun.core.utils.SdkAutoCloseable;
 import com.aliyun.sdk.service.sasti20200512.models.*;
+import darabonba.core.*;
 import darabonba.core.async.*;
 import darabonba.core.sync.*;
 
