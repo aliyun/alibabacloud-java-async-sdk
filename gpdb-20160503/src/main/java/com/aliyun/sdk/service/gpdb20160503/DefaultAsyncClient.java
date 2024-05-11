fcb1ee68aec5a891a8405193f11b79c795cf358a
sdk-team <sdk-team@alibabacloud.com>
2024-05-11 17:08:31 +0000

    Generated java-async 2016-05-03 for gpdb.

--- gpdb-20160503/src/main/java/com/aliyun/sdk/service/gpdb20160503/DefaultAsyncClient.java
+++ gpdb-20160503/src/main/java/com/aliyun/sdk/service/gpdb20160503/DefaultAsyncClient.java
@@ -761,7 +761,7 @@ public final class DefaultAsyncClient implements AsyncClient {
     public CompletableFuture<DescribeDBResourceManagementModeResponse> describeDBResourceManagementMode(DescribeDBResourceManagementModeRequest request) {
         try {
             this.handler.validateRequestModel(request);
-            TeaRequest teaRequest = REQUEST.copy().setStyle(RequestStyle.RPC).setAction("DescribeDBResourceManagementMode").setMethod(HttpMethod.GET).setPathRegex("/").setBodyType(BodyType.JSON).setBodyIsForm(false).setReqBodyType(BodyType.JSON).formModel(request);
+            TeaRequest teaRequest = REQUEST.copy().setStyle(RequestStyle.RPC).setAction("DescribeDBResourceManagementMode").setMethod(HttpMethod.POST).setPathRegex("/").setBodyType(BodyType.JSON).setBodyIsForm(false).setReqBodyType(BodyType.JSON).formModel(request);
             ClientExecutionParams params = new ClientExecutionParams().withInput(request).withRequest(teaRequest).withOutput(DescribeDBResourceManagementModeResponse.create());
             return this.handler.execute(params);
         } catch (Exception e) {
