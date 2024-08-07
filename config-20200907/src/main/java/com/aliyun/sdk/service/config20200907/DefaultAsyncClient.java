e03011162be19ba9798357daf6a8cb4390da3dac
sdk-team <sdk-team@alibabacloud.com>
2024-08-07 17:08:37 +0000

    Generated java-async 2020-09-07 for Config.

--- config-20200907/src/main/java/com/aliyun/sdk/service/config20200907/DefaultAsyncClient.java
+++ config-20200907/src/main/java/com/aliyun/sdk/service/config20200907/DefaultAsyncClient.java
@@ -927,7 +927,7 @@ public final class DefaultAsyncClient implements AsyncClient {
     public CompletableFuture<GetAggregateConfigRuleSummaryByRiskLevelResponse> getAggregateConfigRuleSummaryByRiskLevel(GetAggregateConfigRuleSummaryByRiskLevelRequest request) {
         try {
             this.handler.validateRequestModel(request);
-            TeaRequest teaRequest = REQUEST.copy().setStyle(RequestStyle.RPC).setAction("GetAggregateConfigRuleSummaryByRiskLevel").setMethod(HttpMethod.GET).setPathRegex("/").setBodyType(BodyType.JSON).setBodyIsForm(false).setReqBodyType(BodyType.JSON).formModel(request);
+            TeaRequest teaRequest = REQUEST.copy().setStyle(RequestStyle.RPC).setAction("GetAggregateConfigRuleSummaryByRiskLevel").setMethod(HttpMethod.POST).setPathRegex("/").setBodyType(BodyType.JSON).setBodyIsForm(false).setReqBodyType(BodyType.JSON).formModel(request);
             ClientExecutionParams params = new ClientExecutionParams().withInput(request).withRequest(teaRequest).withOutput(GetAggregateConfigRuleSummaryByRiskLevelResponse.create());
             return this.handler.execute(params);
         } catch (Exception e) {
@@ -1262,7 +1262,7 @@ public final class DefaultAsyncClient implements AsyncClient {
     public CompletableFuture<GetConfigRuleSummaryByRiskLevelResponse> getConfigRuleSummaryByRiskLevel(GetConfigRuleSummaryByRiskLevelRequest request) {
         try {
             this.handler.validateRequestModel(request);
-            TeaRequest teaRequest = REQUEST.copy().setStyle(RequestStyle.RPC).setAction("GetConfigRuleSummaryByRiskLevel").setMethod(HttpMethod.GET).setPathRegex("/").setBodyType(BodyType.JSON).setBodyIsForm(false).setReqBodyType(BodyType.JSON).formModel(request);
+            TeaRequest teaRequest = REQUEST.copy().setStyle(RequestStyle.RPC).setAction("GetConfigRuleSummaryByRiskLevel").setMethod(HttpMethod.POST).setPathRegex("/").setBodyType(BodyType.JSON).setBodyIsForm(false).setReqBodyType(BodyType.JSON).formModel(request);
             ClientExecutionParams params = new ClientExecutionParams().withInput(request).withRequest(teaRequest).withOutput(GetConfigRuleSummaryByRiskLevelResponse.create());
             return this.handler.execute(params);
         } catch (Exception e) {
