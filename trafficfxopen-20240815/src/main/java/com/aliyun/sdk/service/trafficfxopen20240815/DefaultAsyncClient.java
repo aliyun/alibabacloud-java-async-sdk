51bcf1601f7cf3b4c59df55ed0bcd845aac037f9
sdk-team <sdk-team@alibabacloud.com>
2024-08-22 12:34:57 +0000

    Generated java-async 2024-08-15 for TrafficFxOpen.

---
+++ trafficfxopen-20240815/src/main/java/com/aliyun/sdk/service/trafficfxopen20240815/DefaultAsyncClient.java
@@ -0,0 +1,56 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.sdk.service.trafficfxopen20240815;
+
+import com.aliyun.core.http.*;
+import com.aliyun.sdk.service.trafficfxopen20240815.models.*;
+import darabonba.core.utils.*;
+import com.aliyun.sdk.gateway.pop.*;
+import darabonba.core.*;
+import darabonba.core.async.*;
+import darabonba.core.sync.*;
+import darabonba.core.client.*;
+
+import java.util.concurrent.CompletableFuture;
+
+
+/**
+ * <p>Main client.</p>
+ */
+public final class DefaultAsyncClient implements AsyncClient {
+
+    protected final String product;
+    protected final String version;
+    protected final String endpointRule;
+    protected final java.util.Map<String, String> endpointMap;
+    protected final TeaRequest REQUEST;
+    protected final TeaAsyncHandler handler;
+
+    protected DefaultAsyncClient(ClientConfiguration configuration) {
+        this.handler = new TeaAsyncHandler(configuration);
+        this.product = "TrafficFxOpen";
+        this.version = "2024-08-15";
+        this.endpointRule = "";
+        this.endpointMap = new java.util.HashMap<>();
+        this.REQUEST = TeaRequest.create().setProduct(product).setEndpointRule(endpointRule).setEndpointMap(endpointMap).setVersion(version);
+    }
+
+    @Override
+    public void close() {
+        this.handler.close();
+    }
+
+    @Override
+    public CompletableFuture<GetTokenResponse> getToken(GetTokenRequest request) {
+        try {
+            this.handler.validateRequestModel(request);
+            TeaRequest teaRequest = REQUEST.copy().setStyle(RequestStyle.RESTFUL).setAction("GetToken").setMethod(HttpMethod.GET).setPathRegex("/v1/distribution/trade/getToken").setBodyType(BodyType.JSON).setBodyIsForm(false).setReqBodyType(BodyType.JSON).formModel(request);
+            ClientExecutionParams params = new ClientExecutionParams().withInput(request).withRequest(teaRequest).withOutput(GetTokenResponse.create());
+            return this.handler.execute(params);
+        } catch (Exception e) {
+            CompletableFuture<GetTokenResponse> future = new CompletableFuture<>();
+            future.completeExceptionally(e);
+            return future;
+        }
+    }
+
+}
---
+++ trafficfxopen-20240815/src/main/java/com/aliyun/sdk/service/trafficfxopen20240815/DefaultAsyncClientBuilder.java
@@ -0,0 +1,18 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.sdk.service.trafficfxopen20240815;
+
+import com.aliyun.sdk.gateway.pop.BaseClientBuilder;
+
+public final class DefaultAsyncClientBuilder extends BaseClientBuilder<DefaultAsyncClientBuilder, AsyncClient> {
+
+    @Override
+    protected String serviceName() {
+        return "trafficfxopen20240815";
+    }
+
+    @Override
+    protected final AsyncClient buildClient() {
+        return new DefaultAsyncClient(super.applyClientConfiguration());
+    }
+
+}
