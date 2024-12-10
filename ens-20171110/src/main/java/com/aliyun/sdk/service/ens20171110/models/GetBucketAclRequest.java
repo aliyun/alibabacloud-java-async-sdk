e60d1cb8a56ca9920907755e8985cee7b9e97110
sdk-team <sdk-team@alibabacloud.com>
2024-12-10 17:13:30 +0000

    Generated java-async 2017-11-10 for Ens.

--- ens-20171110/src/main/java/com/aliyun/sdk/service/ens20171110/models/GetBucketAclRequest.java
+++ ens-20171110/src/main/java/com/aliyun/sdk/service/ens20171110/models/GetBucketAclRequest.java
@@ -14,6 +14,7 @@ import com.aliyun.sdk.gateway.pop.models.*;
 public class GetBucketAclRequest extends Request {
     @com.aliyun.core.annotation.Query
     @com.aliyun.core.annotation.NameInMap("BucketName")
+    @com.aliyun.core.annotation.Validation(required = true)
     private String bucketName;
 
     private GetBucketAclRequest(Builder builder) {
@@ -55,6 +56,7 @@ public class GetBucketAclRequest extends Request {
 
         /**
          * <p>The name of the bucket.</p>
+         * <p>This parameter is required.</p>
          * 
          * <strong>example:</strong>
          * <p>test</p>
