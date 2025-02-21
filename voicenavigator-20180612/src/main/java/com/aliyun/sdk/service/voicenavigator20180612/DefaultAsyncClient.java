f18453e647a0dfcfe90852d523c9caefc68bdaeb
sdk-team <sdk-team@alibabacloud.com>
2025-02-21 17:14:46 +0000

    Generated java-async 2018-06-12 for VoiceNavigator.

--- voicenavigator-20180612/src/main/java/com/aliyun/sdk/service/voicenavigator20180612/DefaultAsyncClient.java
+++ voicenavigator-20180612/src/main/java/com/aliyun/sdk/service/voicenavigator20180612/DefaultAsyncClient.java
@@ -587,7 +587,7 @@ public final class DefaultAsyncClient implements AsyncClient {
     public CompletableFuture<ListDownloadTasksResponse> listDownloadTasks(ListDownloadTasksRequest request) {
         try {
             this.handler.validateRequestModel(request);
-            TeaRequest teaRequest = REQUEST.copy().setStyle(RequestStyle.RPC).setAction("ListDownloadTasks").setMethod(HttpMethod.GET).setPathRegex("/").setBodyType(BodyType.JSON).setBodyIsForm(false).setReqBodyType(BodyType.JSON).formModel(request);
+            TeaRequest teaRequest = REQUEST.copy().setStyle(RequestStyle.RPC).setAction("ListDownloadTasks").setMethod(HttpMethod.POST).setPathRegex("/").setBodyType(BodyType.JSON).setBodyIsForm(false).setReqBodyType(BodyType.JSON).formModel(request);
             ClientExecutionParams params = new ClientExecutionParams().withInput(request).withRequest(teaRequest).withOutput(ListDownloadTasksResponse.create());
             return this.handler.execute(params);
         } catch (Exception e) {
