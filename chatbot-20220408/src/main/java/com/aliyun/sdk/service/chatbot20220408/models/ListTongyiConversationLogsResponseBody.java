996ba347267f69ca80b121a761a9eb84ec3f98fd
sdk-team <sdk-team@alibabacloud.com>
2025-11-25 17:07:47 +0000

    Generated java-async 2022-04-08 for Chatbot.

--- chatbot-20220408/src/main/java/com/aliyun/sdk/service/chatbot20220408/models/ListTongyiConversationLogsResponseBody.java
+++ chatbot-20220408/src/main/java/com/aliyun/sdk/service/chatbot20220408/models/ListTongyiConversationLogsResponseBody.java
@@ -26,10 +26,14 @@ public class ListTongyiConversationLogsResponseBody extends TeaModel {
     @com.aliyun.core.annotation.NameInMap("RequestId")
     private String requestId;
 
+    @com.aliyun.core.annotation.NameInMap("SessionFlowDebugInfo")
+    private SessionFlowDebugInfo sessionFlowDebugInfo;
+
     private ListTongyiConversationLogsResponseBody(Builder builder) {
         this.costTime = builder.costTime;
         this.datas = builder.datas;
         this.requestId = builder.requestId;
+        this.sessionFlowDebugInfo = builder.sessionFlowDebugInfo;
     }
 
     public static Builder builder() {
@@ -65,10 +69,18 @@ public class ListTongyiConversationLogsResponseBody extends TeaModel {
         return this.requestId;
     }
 
+    /**
+     * @return sessionFlowDebugInfo
+     */
+    public SessionFlowDebugInfo getSessionFlowDebugInfo() {
+        return this.sessionFlowDebugInfo;
+    }
+
     public static final class Builder {
         private String costTime; 
         private java.util.List<java.util.Map<String, ?>> datas; 
         private String requestId; 
+        private SessionFlowDebugInfo sessionFlowDebugInfo; 
 
         private Builder() {
         } 
@@ -77,6 +89,7 @@ public class ListTongyiConversationLogsResponseBody extends TeaModel {
             this.costTime = model.costTime;
             this.datas = model.datas;
             this.requestId = model.requestId;
+            this.sessionFlowDebugInfo = model.sessionFlowDebugInfo;
         } 
 
         /**
@@ -103,10 +116,93 @@ public class ListTongyiConversationLogsResponseBody extends TeaModel {
             return this;
         }
 
+        /**
+         * SessionFlowDebugInfo.
+         */
+        public Builder sessionFlowDebugInfo(SessionFlowDebugInfo sessionFlowDebugInfo) {
+            this.sessionFlowDebugInfo = sessionFlowDebugInfo;
+            return this;
+        }
+
         public ListTongyiConversationLogsResponseBody build() {
             return new ListTongyiConversationLogsResponseBody(this);
         } 
 
     } 
 
+    /**
+     * 
+     * {@link ListTongyiConversationLogsResponseBody} extends {@link TeaModel}
+     *
+     * <p>ListTongyiConversationLogsResponseBody</p>
+     */
+    public static class SessionFlowDebugInfo extends TeaModel {
+        @com.aliyun.core.annotation.NameInMap("ApiParams")
+        private java.util.Map<String, ?> apiParams;
+
+        @com.aliyun.core.annotation.NameInMap("SlotParams")
+        private java.util.Map<String, ?> slotParams;
+
+        private SessionFlowDebugInfo(Builder builder) {
+            this.apiParams = builder.apiParams;
+            this.slotParams = builder.slotParams;
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public static SessionFlowDebugInfo create() {
+            return builder().build();
+        }
+
+        /**
+         * @return apiParams
+         */
+        public java.util.Map<String, ?> getApiParams() {
+            return this.apiParams;
+        }
+
+        /**
+         * @return slotParams
+         */
+        public java.util.Map<String, ?> getSlotParams() {
+            return this.slotParams;
+        }
+
+        public static final class Builder {
+            private java.util.Map<String, ?> apiParams; 
+            private java.util.Map<String, ?> slotParams; 
+
+            private Builder() {
+            } 
+
+            private Builder(SessionFlowDebugInfo model) {
+                this.apiParams = model.apiParams;
+                this.slotParams = model.slotParams;
+            } 
+
+            /**
+             * ApiParams.
+             */
+            public Builder apiParams(java.util.Map<String, ?> apiParams) {
+                this.apiParams = apiParams;
+                return this;
+            }
+
+            /**
+             * SlotParams.
+             */
+            public Builder slotParams(java.util.Map<String, ?> slotParams) {
+                this.slotParams = slotParams;
+                return this;
+            }
+
+            public SessionFlowDebugInfo build() {
+                return new SessionFlowDebugInfo(this);
+            } 
+
+        } 
+
+    }
 }
