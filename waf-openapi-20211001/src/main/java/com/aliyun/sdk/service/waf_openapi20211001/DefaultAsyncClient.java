d5ca5111826473bb726da2f4988ac377aefc947a
sdk-team <sdk-team@alibabacloud.com>
2025-12-08 17:10:53 +0000

    Generated java-async 2021-10-01 for waf-openapi.

--- waf-openapi-20211001/src/main/java/com/aliyun/sdk/service/waf_openapi20211001/DefaultAsyncClient.java
+++ waf-openapi-20211001/src/main/java/com/aliyun/sdk/service/waf_openapi20211001/DefaultAsyncClient.java
@@ -1983,7 +1983,7 @@ public final class DefaultAsyncClient implements AsyncClient {
     public CompletableFuture<DescribeHybridCloudSdkServersResponse> describeHybridCloudSdkServers(DescribeHybridCloudSdkServersRequest request) {
         try {
             this.handler.validateRequestModel(request);
-            TeaRequest teaRequest = REQUEST.copy().setStyle(RequestStyle.RPC).setAction("DescribeHybridCloudSdkServers").setMethod(HttpMethod.GET).setPathRegex("/").setBodyType(BodyType.JSON).setBodyIsForm(false).setReqBodyType(BodyType.JSON).formModel(request);
+            TeaRequest teaRequest = REQUEST.copy().setStyle(RequestStyle.RPC).setAction("DescribeHybridCloudSdkServers").setMethod(HttpMethod.POST).setPathRegex("/").setBodyType(BodyType.JSON).setBodyIsForm(false).setReqBodyType(BodyType.JSON).formModel(request);
             ClientExecutionParams params = new ClientExecutionParams().withInput(request).withRequest(teaRequest).withOutput(DescribeHybridCloudSdkServersResponse.create());
             return this.handler.execute(params);
         } catch (Exception e) {
