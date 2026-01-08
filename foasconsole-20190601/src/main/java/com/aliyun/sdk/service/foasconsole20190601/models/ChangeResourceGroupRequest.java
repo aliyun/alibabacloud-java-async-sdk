e1e4f657601cebb09f2772450ee36dc85697f3b5
sdk-team <sdk-team@alibabacloud.com>
2026-01-08 17:07:57 +0000

    Generated java-async 2019-06-01 for foasconsole.

---
+++ foasconsole-20190601/src/main/java/com/aliyun/sdk/service/foasconsole20190601/models/ChangeResourceGroupRequest.java
@@ -0,0 +1,135 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.sdk.service.foasconsole20190601.models;
+
+import com.aliyun.sdk.gateway.pop.*;
+import darabonba.core.*;
+import darabonba.core.async.*;
+import darabonba.core.sync.*;
+import darabonba.core.client.*;
+import darabonba.core.RequestModel;
+import darabonba.core.TeaModel;
+import com.aliyun.sdk.gateway.pop.models.*;
+
+/**
+ * 
+ * {@link ChangeResourceGroupRequest} extends {@link RequestModel}
+ *
+ * <p>ChangeResourceGroupRequest</p>
+ */
+public class ChangeResourceGroupRequest extends Request {
+    @com.aliyun.core.annotation.Query
+    @com.aliyun.core.annotation.NameInMap("ResourceGroupId")
+    @com.aliyun.core.annotation.Validation(required = true)
+    private String resourceGroupId;
+
+    @com.aliyun.core.annotation.Query
+    @com.aliyun.core.annotation.NameInMap("ResourceId")
+    @com.aliyun.core.annotation.Validation(required = true)
+    private String resourceId;
+
+    @com.aliyun.core.annotation.Query
+    @com.aliyun.core.annotation.NameInMap("ResourceRegionId")
+    @com.aliyun.core.annotation.Validation(required = true)
+    private String resourceRegionId;
+
+    private ChangeResourceGroupRequest(Builder builder) {
+        super(builder);
+        this.resourceGroupId = builder.resourceGroupId;
+        this.resourceId = builder.resourceId;
+        this.resourceRegionId = builder.resourceRegionId;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static ChangeResourceGroupRequest create() {
+        return builder().build();
+    }
+
+@Override
+    public Builder toBuilder() {
+        return new Builder(this);
+    }
+
+    /**
+     * @return resourceGroupId
+     */
+    public String getResourceGroupId() {
+        return this.resourceGroupId;
+    }
+
+    /**
+     * @return resourceId
+     */
+    public String getResourceId() {
+        return this.resourceId;
+    }
+
+    /**
+     * @return resourceRegionId
+     */
+    public String getResourceRegionId() {
+        return this.resourceRegionId;
+    }
+
+    public static final class Builder extends Request.Builder<ChangeResourceGroupRequest, Builder> {
+        private String resourceGroupId; 
+        private String resourceId; 
+        private String resourceRegionId; 
+
+        private Builder() {
+            super();
+        } 
+
+        private Builder(ChangeResourceGroupRequest request) {
+            super(request);
+            this.resourceGroupId = request.resourceGroupId;
+            this.resourceId = request.resourceId;
+            this.resourceRegionId = request.resourceRegionId;
+        } 
+
+        /**
+         * <p>This parameter is required.</p>
+         * 
+         * <strong>example:</strong>
+         * <p>rg-acfmxbavps3rpiy</p>
+         */
+        public Builder resourceGroupId(String resourceGroupId) {
+            this.putQueryParameter("ResourceGroupId", resourceGroupId);
+            this.resourceGroupId = resourceGroupId;
+            return this;
+        }
+
+        /**
+         * <p>This parameter is required.</p>
+         * 
+         * <strong>example:</strong>
+         * <p>f-cn-7e22rg</p>
+         */
+        public Builder resourceId(String resourceId) {
+            this.putQueryParameter("ResourceId", resourceId);
+            this.resourceId = resourceId;
+            return this;
+        }
+
+        /**
+         * <p>This parameter is required.</p>
+         * 
+         * <strong>example:</strong>
+         * <p>us-east-1</p>
+         */
+        public Builder resourceRegionId(String resourceRegionId) {
+            this.putQueryParameter("ResourceRegionId", resourceRegionId);
+            this.resourceRegionId = resourceRegionId;
+            return this;
+        }
+
+        @Override
+        public ChangeResourceGroupRequest build() {
+            return new ChangeResourceGroupRequest(this);
+        } 
+
+    } 
+
+}
