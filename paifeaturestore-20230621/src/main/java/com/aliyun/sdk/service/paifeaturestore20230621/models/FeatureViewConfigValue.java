c96dc44de1484d116997103bfaeeb221ac4d43d3
sdk-team <sdk-team@alibabacloud.com>
2026-01-20 17:11:27 +0000

    Generated java-async 2023-06-21 for PaiFeatureStore.

--- paifeaturestore-20230621/src/main/java/com/aliyun/sdk/service/paifeaturestore20230621/models/FeatureViewConfigValue.java
+++ paifeaturestore-20230621/src/main/java/com/aliyun/sdk/service/paifeaturestore20230621/models/FeatureViewConfigValue.java
@@ -29,11 +29,15 @@ public class FeatureViewConfigValue extends TeaModel {
     @com.aliyun.core.annotation.NameInMap("UseMock")
     private Boolean useMock;
 
+    @com.aliyun.core.annotation.NameInMap("Snapshot")
+    private Snapshot snapshot;
+
     private FeatureViewConfigValue(Builder builder) {
         this.partitions = builder.partitions;
         this.eventTime = builder.eventTime;
         this.equal = builder.equal;
         this.useMock = builder.useMock;
+        this.snapshot = builder.snapshot;
     }
 
     public static Builder builder() {
@@ -76,11 +80,19 @@ public class FeatureViewConfigValue extends TeaModel {
         return this.useMock;
     }
 
+    /**
+     * @return snapshot
+     */
+    public Snapshot getSnapshot() {
+        return this.snapshot;
+    }
+
     public static final class Builder {
         private java.util.Map<String, FeatureViewConfigValuePartitionsValue> partitions; 
         private String eventTime; 
         private Boolean equal; 
         private Boolean useMock; 
+        private Snapshot snapshot; 
 
         private Builder() {
         } 
@@ -90,6 +102,7 @@ public class FeatureViewConfigValue extends TeaModel {
             this.eventTime = model.eventTime;
             this.equal = model.equal;
             this.useMock = model.useMock;
+            this.snapshot = model.snapshot;
         } 
 
         /**
@@ -124,10 +137,93 @@ public class FeatureViewConfigValue extends TeaModel {
             return this;
         }
 
+        /**
+         * Snapshot.
+         */
+        public Builder snapshot(Snapshot snapshot) {
+            this.snapshot = snapshot;
+            return this;
+        }
+
         public FeatureViewConfigValue build() {
             return new FeatureViewConfigValue(this);
         } 
 
     } 
 
+    /**
+     * 
+     * {@link FeatureViewConfigValue} extends {@link TeaModel}
+     *
+     * <p>FeatureViewConfigValue</p>
+     */
+    public static class Snapshot extends TeaModel {
+        @com.aliyun.core.annotation.NameInMap("Partitions")
+        private java.util.Map<String, FeatureViewConfigValueSnapshotPartitionsValue> partitions;
+
+        @com.aliyun.core.annotation.NameInMap("Table")
+        private String table;
+
+        private Snapshot(Builder builder) {
+            this.partitions = builder.partitions;
+            this.table = builder.table;
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public static Snapshot create() {
+            return builder().build();
+        }
+
+        /**
+         * @return partitions
+         */
+        public java.util.Map<String, FeatureViewConfigValueSnapshotPartitionsValue> getPartitions() {
+            return this.partitions;
+        }
+
+        /**
+         * @return table
+         */
+        public String getTable() {
+            return this.table;
+        }
+
+        public static final class Builder {
+            private java.util.Map<String, FeatureViewConfigValueSnapshotPartitionsValue> partitions; 
+            private String table; 
+
+            private Builder() {
+            } 
+
+            private Builder(Snapshot model) {
+                this.partitions = model.partitions;
+                this.table = model.table;
+            } 
+
+            /**
+             * Partitions.
+             */
+            public Builder partitions(java.util.Map<String, FeatureViewConfigValueSnapshotPartitionsValue> partitions) {
+                this.partitions = partitions;
+                return this;
+            }
+
+            /**
+             * Table.
+             */
+            public Builder table(String table) {
+                this.table = table;
+                return this;
+            }
+
+            public Snapshot build() {
+                return new Snapshot(this);
+            } 
+
+        } 
+
+    }
 }
---
+++ paifeaturestore-20230621/src/main/java/com/aliyun/sdk/service/paifeaturestore20230621/models/FeatureViewConfigValueSnapshotPartitionsValue.java
@@ -0,0 +1,133 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.sdk.service.paifeaturestore20230621.models;
+
+import com.aliyun.sdk.gateway.pop.*;
+import darabonba.core.*;
+import darabonba.core.async.*;
+import darabonba.core.sync.*;
+import darabonba.core.client.*;
+import darabonba.core.RequestModel;
+import darabonba.core.TeaModel;
+import com.aliyun.sdk.gateway.pop.models.*;
+
+/**
+ * 
+ * {@link FeatureViewConfigValueSnapshotPartitionsValue} extends {@link TeaModel}
+ *
+ * <p>FeatureViewConfigValueSnapshotPartitionsValue</p>
+ */
+public class FeatureViewConfigValueSnapshotPartitionsValue extends TeaModel {
+    @com.aliyun.core.annotation.NameInMap("Value")
+    private String value;
+
+    @com.aliyun.core.annotation.NameInMap("Values")
+    private java.util.List<String> values;
+
+    @com.aliyun.core.annotation.NameInMap("StartValue")
+    private String startValue;
+
+    @com.aliyun.core.annotation.NameInMap("EndValue")
+    private String endValue;
+
+    private FeatureViewConfigValueSnapshotPartitionsValue(Builder builder) {
+        this.value = builder.value;
+        this.values = builder.values;
+        this.startValue = builder.startValue;
+        this.endValue = builder.endValue;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static FeatureViewConfigValueSnapshotPartitionsValue create() {
+        return builder().build();
+    }
+
+    public Builder toBuilder() {
+        return new Builder(this);
+    }
+
+    /**
+     * @return value
+     */
+    public String getValue() {
+        return this.value;
+    }
+
+    /**
+     * @return values
+     */
+    public java.util.List<String> getValues() {
+        return this.values;
+    }
+
+    /**
+     * @return startValue
+     */
+    public String getStartValue() {
+        return this.startValue;
+    }
+
+    /**
+     * @return endValue
+     */
+    public String getEndValue() {
+        return this.endValue;
+    }
+
+    public static final class Builder {
+        private String value; 
+        private java.util.List<String> values; 
+        private String startValue; 
+        private String endValue; 
+
+        private Builder() {
+        } 
+
+        private Builder(FeatureViewConfigValueSnapshotPartitionsValue model) {
+            this.value = model.value;
+            this.values = model.values;
+            this.startValue = model.startValue;
+            this.endValue = model.endValue;
+        } 
+
+        /**
+         * Value.
+         */
+        public Builder value(String value) {
+            this.value = value;
+            return this;
+        }
+
+        /**
+         * Values.
+         */
+        public Builder values(java.util.List<String> values) {
+            this.values = values;
+            return this;
+        }
+
+        /**
+         * StartValue.
+         */
+        public Builder startValue(String startValue) {
+            this.startValue = startValue;
+            return this;
+        }
+
+        /**
+         * EndValue.
+         */
+        public Builder endValue(String endValue) {
+            this.endValue = endValue;
+            return this;
+        }
+
+        public FeatureViewConfigValueSnapshotPartitionsValue build() {
+            return new FeatureViewConfigValueSnapshotPartitionsValue(this);
+        } 
+
+    } 
+
+}
