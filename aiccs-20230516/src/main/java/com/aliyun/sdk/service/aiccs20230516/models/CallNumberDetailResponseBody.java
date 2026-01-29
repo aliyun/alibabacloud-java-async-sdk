1a019ab577c403e7ad9bf318b72e2cd70396916f
sdk-team <sdk-team@alibabacloud.com>
2026-01-29 12:44:55 +0000

    Generated java-async 2023-05-16 for aiccs.

--- aiccs-20230516/src/main/java/com/aliyun/sdk/service/aiccs20230516/models/CallNumberDetailResponseBody.java
+++ aiccs-20230516/src/main/java/com/aliyun/sdk/service/aiccs20230516/models/CallNumberDetailResponseBody.java
@@ -200,17 +200,38 @@ public class CallNumberDetailResponseBody extends TeaModel {
      * <p>CallNumberDetailResponseBody</p>
      */
     public static class Model extends TeaModel {
+        @com.aliyun.core.annotation.NameInMap("AiBill")
+        private Long aiBill;
+
+        @com.aliyun.core.annotation.NameInMap("AnswerTransferType")
+        private Long answerTransferType;
+
         @com.aliyun.core.annotation.NameInMap("BatchId")
         private String batchId;
 
         @com.aliyun.core.annotation.NameInMap("Bill")
         private Long bill;
 
+        @com.aliyun.core.annotation.NameInMap("BridgeBill")
+        private Long bridgeBill;
+
         @com.aliyun.core.annotation.NameInMap("CallId")
         private String callId;
 
         @com.aliyun.core.annotation.NameInMap("CallType")
         private Long callType;
+
+        @com.aliyun.core.annotation.NameInMap("ClientUrl")
+        private String clientUrl;
+
+        @com.aliyun.core.annotation.NameInMap("CreateTime")
+        private String createTime;
+
+        @com.aliyun.core.annotation.NameInMap("Duration")
+        private Long duration;
+
+        @com.aliyun.core.annotation.NameInMap("GatewayId")
+        private Long gatewayId;
 
         @com.aliyun.core.annotation.NameInMap("Id")
         private Long id;
@@ -227,8 +248,17 @@ public class CallNumberDetailResponseBody extends TeaModel {
         @com.aliyun.core.annotation.NameInMap("NumberMd5")
         private String numberMd5;
 
+        @com.aliyun.core.annotation.NameInMap("Params")
+        private String params;
+
         @com.aliyun.core.annotation.NameInMap("PersonalityTag")
         private String personalityTag;
+
+        @com.aliyun.core.annotation.NameInMap("Remark")
+        private String remark;
+
+        @com.aliyun.core.annotation.NameInMap("Sid")
+        private Long sid;
 
         @com.aliyun.core.annotation.NameInMap("StatusCode")
         private Long statusCode;
@@ -246,16 +276,26 @@ public class CallNumberDetailResponseBody extends TeaModel {
         private Long transferStatus;
 
         private Model(Builder builder) {
+            this.aiBill = builder.aiBill;
+            this.answerTransferType = builder.answerTransferType;
             this.batchId = builder.batchId;
             this.bill = builder.bill;
+            this.bridgeBill = builder.bridgeBill;
             this.callId = builder.callId;
             this.callType = builder.callType;
+            this.clientUrl = builder.clientUrl;
+            this.createTime = builder.createTime;
+            this.duration = builder.duration;
+            this.gatewayId = builder.gatewayId;
             this.id = builder.id;
             this.intentTag = builder.intentTag;
             this.keywords = builder.keywords;
             this.number = builder.number;
             this.numberMd5 = builder.numberMd5;
+            this.params = builder.params;
             this.personalityTag = builder.personalityTag;
+            this.remark = builder.remark;
+            this.sid = builder.sid;
             this.statusCode = builder.statusCode;
             this.tag = builder.tag;
             this.taskId = builder.taskId;
@@ -269,6 +309,20 @@ public class CallNumberDetailResponseBody extends TeaModel {
 
         public static Model create() {
             return builder().build();
+        }
+
+        /**
+         * @return aiBill
+         */
+        public Long getAiBill() {
+            return this.aiBill;
+        }
+
+        /**
+         * @return answerTransferType
+         */
+        public Long getAnswerTransferType() {
+            return this.answerTransferType;
         }
 
         /**
@@ -286,6 +340,13 @@ public class CallNumberDetailResponseBody extends TeaModel {
         }
 
         /**
+         * @return bridgeBill
+         */
+        public Long getBridgeBill() {
+            return this.bridgeBill;
+        }
+
+        /**
          * @return callId
          */
         public String getCallId() {
@@ -297,6 +358,34 @@ public class CallNumberDetailResponseBody extends TeaModel {
          */
         public Long getCallType() {
             return this.callType;
+        }
+
+        /**
+         * @return clientUrl
+         */
+        public String getClientUrl() {
+            return this.clientUrl;
+        }
+
+        /**
+         * @return createTime
+         */
+        public String getCreateTime() {
+            return this.createTime;
+        }
+
+        /**
+         * @return duration
+         */
+        public Long getDuration() {
+            return this.duration;
+        }
+
+        /**
+         * @return gatewayId
+         */
+        public Long getGatewayId() {
+            return this.gatewayId;
         }
 
         /**
@@ -335,10 +424,31 @@ public class CallNumberDetailResponseBody extends TeaModel {
         }
 
         /**
+         * @return params
+         */
+        public String getParams() {
+            return this.params;
+        }
+
+        /**
          * @return personalityTag
          */
         public String getPersonalityTag() {
             return this.personalityTag;
+        }
+
+        /**
+         * @return remark
+         */
+        public String getRemark() {
+            return this.remark;
+        }
+
+        /**
+         * @return sid
+         */
+        public Long getSid() {
+            return this.sid;
         }
 
         /**
@@ -377,16 +487,26 @@ public class CallNumberDetailResponseBody extends TeaModel {
         }
 
         public static final class Builder {
+            private Long aiBill; 
+            private Long answerTransferType; 
             private String batchId; 
             private Long bill; 
+            private Long bridgeBill; 
             private String callId; 
             private Long callType; 
+            private String clientUrl; 
+            private String createTime; 
+            private Long duration; 
+            private Long gatewayId; 
             private Long id; 
             private String intentTag; 
             private String keywords; 
             private String number; 
             private String numberMd5; 
+            private String params; 
             private String personalityTag; 
+            private String remark; 
+            private Long sid; 
             private Long statusCode; 
             private String tag; 
             private Long taskId; 
@@ -397,22 +517,54 @@ public class CallNumberDetailResponseBody extends TeaModel {
             } 
 
             private Builder(Model model) {
+                this.aiBill = model.aiBill;
+                this.answerTransferType = model.answerTransferType;
                 this.batchId = model.batchId;
                 this.bill = model.bill;
+                this.bridgeBill = model.bridgeBill;
                 this.callId = model.callId;
                 this.callType = model.callType;
+                this.clientUrl = model.clientUrl;
+                this.createTime = model.createTime;
+                this.duration = model.duration;
+                this.gatewayId = model.gatewayId;
                 this.id = model.id;
                 this.intentTag = model.intentTag;
                 this.keywords = model.keywords;
                 this.number = model.number;
                 this.numberMd5 = model.numberMd5;
+                this.params = model.params;
                 this.personalityTag = model.personalityTag;
+                this.remark = model.remark;
+                this.sid = model.sid;
                 this.statusCode = model.statusCode;
                 this.tag = model.tag;
                 this.taskId = model.taskId;
                 this.templateId = model.templateId;
                 this.transferStatus = model.transferStatus;
             } 
+
+            /**
+             * <p>ai计费时长</p>
+             * 
+             * <strong>example:</strong>
+             * <p>1</p>
+             */
+            public Builder aiBill(Long aiBill) {
+                this.aiBill = aiBill;
+                return this;
+            }
+
+            /**
+             * <p>接通转接类型 1-不转人工；2-接通转人工；3-智能转人工</p>
+             * 
+             * <strong>example:</strong>
+             * <p>1</p>
+             */
+            public Builder answerTransferType(Long answerTransferType) {
+                this.answerTransferType = answerTransferType;
+                return this;
+            }
 
             /**
              * <p>导入号码时返回的批次号</p>
@@ -437,6 +589,17 @@ public class CallNumberDetailResponseBody extends TeaModel {
             }
 
             /**
+             * <p>转接计费时长</p>
+             * 
+             * <strong>example:</strong>
+             * <p>1</p>
+             */
+            public Builder bridgeBill(Long bridgeBill) {
+                this.bridgeBill = bridgeBill;
+                return this;
+            }
+
+            /**
              * <p>每次呼叫的唯一标识</p>
              * 
              * <strong>example:</strong>
@@ -455,6 +618,50 @@ public class CallNumberDetailResponseBody extends TeaModel {
              */
             public Builder callType(Long callType) {
                 this.callType = callType;
+                return this;
+            }
+
+            /**
+             * <p>客户详情url</p>
+             * 
+             * <strong>example:</strong>
+             * <p>a</p>
+             */
+            public Builder clientUrl(String clientUrl) {
+                this.clientUrl = clientUrl;
+                return this;
+            }
+
+            /**
+             * <p>创建时间 格式 2026-01-01 00:00:00</p>
+             * 
+             * <strong>example:</strong>
+             * <p>2026-01-01 00:00:00</p>
+             */
+            public Builder createTime(String createTime) {
+                this.createTime = createTime;
+                return this;
+            }
+
+            /**
+             * <p>通话轮次</p>
+             * 
+             * <strong>example:</strong>
+             * <p>1</p>
+             */
+            public Builder duration(Long duration) {
+                this.duration = duration;
+                return this;
+            }
+
+            /**
+             * <p>线路id</p>
+             * 
+             * <strong>example:</strong>
+             * <p>1</p>
+             */
+            public Builder gatewayId(Long gatewayId) {
+                this.gatewayId = gatewayId;
                 return this;
             }
 
@@ -514,6 +721,17 @@ public class CallNumberDetailResponseBody extends TeaModel {
             }
 
             /**
+             * <p>自定义参数 json</p>
+             * 
+             * <strong>example:</strong>
+             * <p>{&quot;名字&quot;:&quot;a&quot;}</p>
+             */
+            public Builder params(String params) {
+                this.params = params;
+                return this;
+            }
+
+            /**
              * <p>个性标签</p>
              * 
              * <strong>example:</strong>
@@ -521,6 +739,28 @@ public class CallNumberDetailResponseBody extends TeaModel {
              */
             public Builder personalityTag(String personalityTag) {
                 this.personalityTag = personalityTag;
+                return this;
+            }
+
+            /**
+             * <p>备注信息</p>
+             * 
+             * <strong>example:</strong>
+             * <p>a</p>
+             */
+            public Builder remark(String remark) {
+                this.remark = remark;
+                return this;
+            }
+
+            /**
+             * <p>坐席id</p>
+             * 
+             * <strong>example:</strong>
+             * <p>1</p>
+             */
+            public Builder sid(Long sid) {
+                this.sid = sid;
                 return this;
             }
 
