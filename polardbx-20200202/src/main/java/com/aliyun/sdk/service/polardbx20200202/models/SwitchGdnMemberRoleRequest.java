5712f63078a7aa9d764014497f7cbdf416497721
sdk-team <sdk-team@alibabacloud.com>
2026-03-16 17:12:57 +0000

    Generated java-async 2020-02-02 for polardbx.

--- polardbx-20200202/src/main/java/com/aliyun/sdk/service/polardbx20200202/models/SwitchGdnMemberRoleRequest.java
+++ polardbx-20200202/src/main/java/com/aliyun/sdk/service/polardbx20200202/models/SwitchGdnMemberRoleRequest.java
@@ -23,9 +23,29 @@ public class SwitchGdnMemberRoleRequest extends Request {
     private String DBInstanceName;
 
     @com.aliyun.core.annotation.Query
+    @com.aliyun.core.annotation.NameInMap("DstMainConnectString")
+    private String dstMainConnectString;
+
+    @com.aliyun.core.annotation.Query
+    @com.aliyun.core.annotation.NameInMap("DstMainPort")
+    private String dstMainPort;
+
+    @com.aliyun.core.annotation.Query
+    @com.aliyun.core.annotation.NameInMap("IsModifyEndpoint")
+    private String isModifyEndpoint;
+
+    @com.aliyun.core.annotation.Query
     @com.aliyun.core.annotation.NameInMap("RegionId")
     @com.aliyun.core.annotation.Validation(required = true)
     private String regionId;
+
+    @com.aliyun.core.annotation.Query
+    @com.aliyun.core.annotation.NameInMap("SrcMainConnectString")
+    private String srcMainConnectString;
+
+    @com.aliyun.core.annotation.Query
+    @com.aliyun.core.annotation.NameInMap("SrcMainPort")
+    private String srcMainPort;
 
     @com.aliyun.core.annotation.Query
     @com.aliyun.core.annotation.NameInMap("SwitchMode")
@@ -39,7 +59,12 @@ public class SwitchGdnMemberRoleRequest extends Request {
     private SwitchGdnMemberRoleRequest(Builder builder) {
         super(builder);
         this.DBInstanceName = builder.DBInstanceName;
+        this.dstMainConnectString = builder.dstMainConnectString;
+        this.dstMainPort = builder.dstMainPort;
+        this.isModifyEndpoint = builder.isModifyEndpoint;
         this.regionId = builder.regionId;
+        this.srcMainConnectString = builder.srcMainConnectString;
+        this.srcMainPort = builder.srcMainPort;
         this.switchMode = builder.switchMode;
         this.taskTimeout = builder.taskTimeout;
     }
@@ -65,10 +90,45 @@ public class SwitchGdnMemberRoleRequest extends Request {
     }
 
     /**
+     * @return dstMainConnectString
+     */
+    public String getDstMainConnectString() {
+        return this.dstMainConnectString;
+    }
+
+    /**
+     * @return dstMainPort
+     */
+    public String getDstMainPort() {
+        return this.dstMainPort;
+    }
+
+    /**
+     * @return isModifyEndpoint
+     */
+    public String getIsModifyEndpoint() {
+        return this.isModifyEndpoint;
+    }
+
+    /**
      * @return regionId
      */
     public String getRegionId() {
         return this.regionId;
+    }
+
+    /**
+     * @return srcMainConnectString
+     */
+    public String getSrcMainConnectString() {
+        return this.srcMainConnectString;
+    }
+
+    /**
+     * @return srcMainPort
+     */
+    public String getSrcMainPort() {
+        return this.srcMainPort;
     }
 
     /**
@@ -87,7 +147,12 @@ public class SwitchGdnMemberRoleRequest extends Request {
 
     public static final class Builder extends Request.Builder<SwitchGdnMemberRoleRequest, Builder> {
         private String DBInstanceName; 
+        private String dstMainConnectString; 
+        private String dstMainPort; 
+        private String isModifyEndpoint; 
         private String regionId; 
+        private String srcMainConnectString; 
+        private String srcMainPort; 
         private String switchMode; 
         private Long taskTimeout; 
 
@@ -98,7 +163,12 @@ public class SwitchGdnMemberRoleRequest extends Request {
         private Builder(SwitchGdnMemberRoleRequest request) {
             super(request);
             this.DBInstanceName = request.DBInstanceName;
+            this.dstMainConnectString = request.dstMainConnectString;
+            this.dstMainPort = request.dstMainPort;
+            this.isModifyEndpoint = request.isModifyEndpoint;
             this.regionId = request.regionId;
+            this.srcMainConnectString = request.srcMainConnectString;
+            this.srcMainPort = request.srcMainPort;
             this.switchMode = request.switchMode;
             this.taskTimeout = request.taskTimeout;
         } 
@@ -116,6 +186,33 @@ public class SwitchGdnMemberRoleRequest extends Request {
         }
 
         /**
+         * DstMainConnectString.
+         */
+        public Builder dstMainConnectString(String dstMainConnectString) {
+            this.putQueryParameter("DstMainConnectString", dstMainConnectString);
+            this.dstMainConnectString = dstMainConnectString;
+            return this;
+        }
+
+        /**
+         * DstMainPort.
+         */
+        public Builder dstMainPort(String dstMainPort) {
+            this.putQueryParameter("DstMainPort", dstMainPort);
+            this.dstMainPort = dstMainPort;
+            return this;
+        }
+
+        /**
+         * IsModifyEndpoint.
+         */
+        public Builder isModifyEndpoint(String isModifyEndpoint) {
+            this.putQueryParameter("IsModifyEndpoint", isModifyEndpoint);
+            this.isModifyEndpoint = isModifyEndpoint;
+            return this;
+        }
+
+        /**
          * <p>This parameter is required.</p>
          * 
          * <strong>example:</strong>
@@ -124,6 +221,24 @@ public class SwitchGdnMemberRoleRequest extends Request {
         public Builder regionId(String regionId) {
             this.putQueryParameter("RegionId", regionId);
             this.regionId = regionId;
+            return this;
+        }
+
+        /**
+         * SrcMainConnectString.
+         */
+        public Builder srcMainConnectString(String srcMainConnectString) {
+            this.putQueryParameter("SrcMainConnectString", srcMainConnectString);
+            this.srcMainConnectString = srcMainConnectString;
+            return this;
+        }
+
+        /**
+         * SrcMainPort.
+         */
+        public Builder srcMainPort(String srcMainPort) {
+            this.putQueryParameter("SrcMainPort", srcMainPort);
+            this.srcMainPort = srcMainPort;
             return this;
         }
 
