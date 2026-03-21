f01196464979ae8a947de56660bb7c294746277b
sdk-team <sdk-team@alibabacloud.com>
2026-03-21 17:08:21 +0000

    Generated java-async 2023-09-30 for eds-aic.

--- eds-aic-20230930/src/main/java/com/aliyun/sdk/service/eds_aic20230930/models/RebootAndroidInstancesInGroupResponseBody.java
+++ eds-aic-20230930/src/main/java/com/aliyun/sdk/service/eds_aic20230930/models/RebootAndroidInstancesInGroupResponseBody.java
@@ -20,8 +20,12 @@ public class RebootAndroidInstancesInGroupResponseBody extends TeaModel {
     @com.aliyun.core.annotation.NameInMap("RequestId")
     private String requestId;
 
+    @com.aliyun.core.annotation.NameInMap("Tasks")
+    private Tasks tasks;
+
     private RebootAndroidInstancesInGroupResponseBody(Builder builder) {
         this.requestId = builder.requestId;
+        this.tasks = builder.tasks;
     }
 
     public static Builder builder() {
@@ -43,14 +47,23 @@ public class RebootAndroidInstancesInGroupResponseBody extends TeaModel {
         return this.requestId;
     }
 
+    /**
+     * @return tasks
+     */
+    public Tasks getTasks() {
+        return this.tasks;
+    }
+
     public static final class Builder {
         private String requestId; 
+        private Tasks tasks; 
 
         private Builder() {
         } 
 
         private Builder(RebootAndroidInstancesInGroupResponseBody model) {
             this.requestId = model.requestId;
+            this.tasks = model.tasks;
         } 
 
         /**
@@ -64,10 +77,168 @@ public class RebootAndroidInstancesInGroupResponseBody extends TeaModel {
             return this;
         }
 
+        /**
+         * Tasks.
+         */
+        public Builder tasks(Tasks tasks) {
+            this.tasks = tasks;
+            return this;
+        }
+
         public RebootAndroidInstancesInGroupResponseBody build() {
             return new RebootAndroidInstancesInGroupResponseBody(this);
         } 
 
     } 
 
+    /**
+     * 
+     * {@link RebootAndroidInstancesInGroupResponseBody} extends {@link TeaModel}
+     *
+     * <p>RebootAndroidInstancesInGroupResponseBody</p>
+     */
+    public static class ChildTasks extends TeaModel {
+        @com.aliyun.core.annotation.NameInMap("InstanceId")
+        private String instanceId;
+
+        @com.aliyun.core.annotation.NameInMap("TaskId")
+        private String taskId;
+
+        private ChildTasks(Builder builder) {
+            this.instanceId = builder.instanceId;
+            this.taskId = builder.taskId;
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public static ChildTasks create() {
+            return builder().build();
+        }
+
+        /**
+         * @return instanceId
+         */
+        public String getInstanceId() {
+            return this.instanceId;
+        }
+
+        /**
+         * @return taskId
+         */
+        public String getTaskId() {
+            return this.taskId;
+        }
+
+        public static final class Builder {
+            private String instanceId; 
+            private String taskId; 
+
+            private Builder() {
+            } 
+
+            private Builder(ChildTasks model) {
+                this.instanceId = model.instanceId;
+                this.taskId = model.taskId;
+            } 
+
+            /**
+             * InstanceId.
+             */
+            public Builder instanceId(String instanceId) {
+                this.instanceId = instanceId;
+                return this;
+            }
+
+            /**
+             * TaskId.
+             */
+            public Builder taskId(String taskId) {
+                this.taskId = taskId;
+                return this;
+            }
+
+            public ChildTasks build() {
+                return new ChildTasks(this);
+            } 
+
+        } 
+
+    }
+    /**
+     * 
+     * {@link RebootAndroidInstancesInGroupResponseBody} extends {@link TeaModel}
+     *
+     * <p>RebootAndroidInstancesInGroupResponseBody</p>
+     */
+    public static class Tasks extends TeaModel {
+        @com.aliyun.core.annotation.NameInMap("ChildTasks")
+        private java.util.List<ChildTasks> childTasks;
+
+        @com.aliyun.core.annotation.NameInMap("ParentTaskId")
+        private String parentTaskId;
+
+        private Tasks(Builder builder) {
+            this.childTasks = builder.childTasks;
+            this.parentTaskId = builder.parentTaskId;
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public static Tasks create() {
+            return builder().build();
+        }
+
+        /**
+         * @return childTasks
+         */
+        public java.util.List<ChildTasks> getChildTasks() {
+            return this.childTasks;
+        }
+
+        /**
+         * @return parentTaskId
+         */
+        public String getParentTaskId() {
+            return this.parentTaskId;
+        }
+
+        public static final class Builder {
+            private java.util.List<ChildTasks> childTasks; 
+            private String parentTaskId; 
+
+            private Builder() {
+            } 
+
+            private Builder(Tasks model) {
+                this.childTasks = model.childTasks;
+                this.parentTaskId = model.parentTaskId;
+            } 
+
+            /**
+             * ChildTasks.
+             */
+            public Builder childTasks(java.util.List<ChildTasks> childTasks) {
+                this.childTasks = childTasks;
+                return this;
+            }
+
+            /**
+             * ParentTaskId.
+             */
+            public Builder parentTaskId(String parentTaskId) {
+                this.parentTaskId = parentTaskId;
+                return this;
+            }
+
+            public Tasks build() {
+                return new Tasks(this);
+            } 
+
+        } 
+
+    }
 }
--- eds-aic-20230930/src/main/java/com/aliyun/sdk/service/eds_aic20230930/models/UpdateInstanceImageResponseBody.java
+++ eds-aic-20230930/src/main/java/com/aliyun/sdk/service/eds_aic20230930/models/UpdateInstanceImageResponseBody.java
@@ -23,9 +23,13 @@ public class UpdateInstanceImageResponseBody extends TeaModel {
     @com.aliyun.core.annotation.NameInMap("TaskId")
     private String taskId;
 
+    @com.aliyun.core.annotation.NameInMap("Tasks")
+    private Tasks tasks;
+
     private UpdateInstanceImageResponseBody(Builder builder) {
         this.requestId = builder.requestId;
         this.taskId = builder.taskId;
+        this.tasks = builder.tasks;
     }
 
     public static Builder builder() {
@@ -54,9 +58,17 @@ public class UpdateInstanceImageResponseBody extends TeaModel {
         return this.taskId;
     }
 
+    /**
+     * @return tasks
+     */
+    public Tasks getTasks() {
+        return this.tasks;
+    }
+
     public static final class Builder {
         private String requestId; 
         private String taskId; 
+        private Tasks tasks; 
 
         private Builder() {
         } 
@@ -64,6 +76,7 @@ public class UpdateInstanceImageResponseBody extends TeaModel {
         private Builder(UpdateInstanceImageResponseBody model) {
             this.requestId = model.requestId;
             this.taskId = model.taskId;
+            this.tasks = model.tasks;
         } 
 
         /**
@@ -82,10 +95,168 @@ public class UpdateInstanceImageResponseBody extends TeaModel {
             return this;
         }
 
+        /**
+         * Tasks.
+         */
+        public Builder tasks(Tasks tasks) {
+            this.tasks = tasks;
+            return this;
+        }
+
         public UpdateInstanceImageResponseBody build() {
             return new UpdateInstanceImageResponseBody(this);
         } 
 
     } 
 
+    /**
+     * 
+     * {@link UpdateInstanceImageResponseBody} extends {@link TeaModel}
+     *
+     * <p>UpdateInstanceImageResponseBody</p>
+     */
+    public static class ChildTasks extends TeaModel {
+        @com.aliyun.core.annotation.NameInMap("InstanceId")
+        private String instanceId;
+
+        @com.aliyun.core.annotation.NameInMap("TaskId")
+        private String taskId;
+
+        private ChildTasks(Builder builder) {
+            this.instanceId = builder.instanceId;
+            this.taskId = builder.taskId;
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public static ChildTasks create() {
+            return builder().build();
+        }
+
+        /**
+         * @return instanceId
+         */
+        public String getInstanceId() {
+            return this.instanceId;
+        }
+
+        /**
+         * @return taskId
+         */
+        public String getTaskId() {
+            return this.taskId;
+        }
+
+        public static final class Builder {
+            private String instanceId; 
+            private String taskId; 
+
+            private Builder() {
+            } 
+
+            private Builder(ChildTasks model) {
+                this.instanceId = model.instanceId;
+                this.taskId = model.taskId;
+            } 
+
+            /**
+             * InstanceId.
+             */
+            public Builder instanceId(String instanceId) {
+                this.instanceId = instanceId;
+                return this;
+            }
+
+            /**
+             * TaskId.
+             */
+            public Builder taskId(String taskId) {
+                this.taskId = taskId;
+                return this;
+            }
+
+            public ChildTasks build() {
+                return new ChildTasks(this);
+            } 
+
+        } 
+
+    }
+    /**
+     * 
+     * {@link UpdateInstanceImageResponseBody} extends {@link TeaModel}
+     *
+     * <p>UpdateInstanceImageResponseBody</p>
+     */
+    public static class Tasks extends TeaModel {
+        @com.aliyun.core.annotation.NameInMap("ChildTasks")
+        private java.util.List<ChildTasks> childTasks;
+
+        @com.aliyun.core.annotation.NameInMap("ParentTaskId")
+        private String parentTaskId;
+
+        private Tasks(Builder builder) {
+            this.childTasks = builder.childTasks;
+            this.parentTaskId = builder.parentTaskId;
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public static Tasks create() {
+            return builder().build();
+        }
+
+        /**
+         * @return childTasks
+         */
+        public java.util.List<ChildTasks> getChildTasks() {
+            return this.childTasks;
+        }
+
+        /**
+         * @return parentTaskId
+         */
+        public String getParentTaskId() {
+            return this.parentTaskId;
+        }
+
+        public static final class Builder {
+            private java.util.List<ChildTasks> childTasks; 
+            private String parentTaskId; 
+
+            private Builder() {
+            } 
+
+            private Builder(Tasks model) {
+                this.childTasks = model.childTasks;
+                this.parentTaskId = model.parentTaskId;
+            } 
+
+            /**
+             * ChildTasks.
+             */
+            public Builder childTasks(java.util.List<ChildTasks> childTasks) {
+                this.childTasks = childTasks;
+                return this;
+            }
+
+            /**
+             * ParentTaskId.
+             */
+            public Builder parentTaskId(String parentTaskId) {
+                this.parentTaskId = parentTaskId;
+                return this;
+            }
+
+            public Tasks build() {
+                return new Tasks(this);
+            } 
+
+        } 
+
+    }
 }
