ed9ed8a0cc4ec800fe61083a96efd6435ea317de
sdk-team <sdk-team@alibabacloud.com>
2026-08-20 01:57:56 +0000

    Generated java-async 2026-04-24 for RiskManagement.

--- riskmanagement-20260424/src/main/java/com/aliyun/sdk/service/riskmanagement20260424/models/QueryAccountSafetyIncidentRequest.java
+++ riskmanagement-20260424/src/main/java/com/aliyun/sdk/service/riskmanagement20260424/models/QueryAccountSafetyIncidentRequest.java
@@ -18,6 +18,10 @@ import com.aliyun.sdk.gateway.pop.models.*;
  */
 public class QueryAccountSafetyIncidentRequest extends Request {
     @com.aliyun.core.annotation.Query
+    @com.aliyun.core.annotation.NameInMap("ActionCodes")
+    private java.util.List<String> actionCodes;
+
+    @com.aliyun.core.annotation.Query
     @com.aliyun.core.annotation.NameInMap("AliyunLang")
     private String aliyunLang;
 
@@ -26,12 +30,20 @@ public class QueryAccountSafetyIncidentRequest extends Request {
     private String caseCode;
 
     @com.aliyun.core.annotation.Query
+    @com.aliyun.core.annotation.NameInMap("CaseCodes")
+    private java.util.List<String> caseCodes;
+
+    @com.aliyun.core.annotation.Query
     @com.aliyun.core.annotation.NameInMap("Current")
     private String current;
 
     @com.aliyun.core.annotation.Query
     @com.aliyun.core.annotation.NameInMap("EventId")
     private String eventId;
+
+    @com.aliyun.core.annotation.Query
+    @com.aliyun.core.annotation.NameInMap("EventIds")
+    private java.util.List<String> eventIds;
 
     @com.aliyun.core.annotation.Query
     @com.aliyun.core.annotation.NameInMap("PageSize")
@@ -53,17 +65,25 @@ public class QueryAccountSafetyIncidentRequest extends Request {
     @com.aliyun.core.annotation.NameInMap("Status")
     private String status;
 
+    @com.aliyun.core.annotation.Query
+    @com.aliyun.core.annotation.NameInMap("Statuses")
+    private java.util.List<String> statuses;
+
     private QueryAccountSafetyIncidentRequest(Builder builder) {
         super(builder);
+        this.actionCodes = builder.actionCodes;
         this.aliyunLang = builder.aliyunLang;
         this.caseCode = builder.caseCode;
+        this.caseCodes = builder.caseCodes;
         this.current = builder.current;
         this.eventId = builder.eventId;
+        this.eventIds = builder.eventIds;
         this.pageSize = builder.pageSize;
         this.punishEndTime = builder.punishEndTime;
         this.punishStartTime = builder.punishStartTime;
         this.resourceId = builder.resourceId;
         this.status = builder.status;
+        this.statuses = builder.statuses;
     }
 
     public static Builder builder() {
@@ -77,6 +97,13 @@ public class QueryAccountSafetyIncidentRequest extends Request {
 @Override
     public Builder toBuilder() {
         return new Builder(this);
+    }
+
+    /**
+     * @return actionCodes
+     */
+    public java.util.List<String> getActionCodes() {
+        return this.actionCodes;
     }
 
     /**
@@ -94,6 +121,13 @@ public class QueryAccountSafetyIncidentRequest extends Request {
     }
 
     /**
+     * @return caseCodes
+     */
+    public java.util.List<String> getCaseCodes() {
+        return this.caseCodes;
+    }
+
+    /**
      * @return current
      */
     public String getCurrent() {
@@ -105,6 +139,13 @@ public class QueryAccountSafetyIncidentRequest extends Request {
      */
     public String getEventId() {
         return this.eventId;
+    }
+
+    /**
+     * @return eventIds
+     */
+    public java.util.List<String> getEventIds() {
+        return this.eventIds;
     }
 
     /**
@@ -142,16 +183,27 @@ public class QueryAccountSafetyIncidentRequest extends Request {
         return this.status;
     }
 
+    /**
+     * @return statuses
+     */
+    public java.util.List<String> getStatuses() {
+        return this.statuses;
+    }
+
     public static final class Builder extends Request.Builder<QueryAccountSafetyIncidentRequest, Builder> {
+        private java.util.List<String> actionCodes; 
         private String aliyunLang; 
         private String caseCode; 
+        private java.util.List<String> caseCodes; 
         private String current; 
         private String eventId; 
+        private java.util.List<String> eventIds; 
         private String pageSize; 
         private String punishEndTime; 
         private String punishStartTime; 
         private String resourceId; 
         private String status; 
+        private java.util.List<String> statuses; 
 
         private Builder() {
             super();
@@ -159,16 +211,30 @@ public class QueryAccountSafetyIncidentRequest extends Request {
 
         private Builder(QueryAccountSafetyIncidentRequest request) {
             super(request);
+            this.actionCodes = request.actionCodes;
             this.aliyunLang = request.aliyunLang;
             this.caseCode = request.caseCode;
+            this.caseCodes = request.caseCodes;
             this.current = request.current;
             this.eventId = request.eventId;
+            this.eventIds = request.eventIds;
             this.pageSize = request.pageSize;
             this.punishEndTime = request.punishEndTime;
             this.punishStartTime = request.punishStartTime;
             this.resourceId = request.resourceId;
             this.status = request.status;
+            this.statuses = request.statuses;
         } 
+
+        /**
+         * ActionCodes.
+         */
+        public Builder actionCodes(java.util.List<String> actionCodes) {
+            String actionCodesShrink = shrink(actionCodes, "ActionCodes", "json");
+            this.putQueryParameter("ActionCodes", actionCodesShrink);
+            this.actionCodes = actionCodes;
+            return this;
+        }
 
         /**
          * AliyunLang.
@@ -189,6 +255,16 @@ public class QueryAccountSafetyIncidentRequest extends Request {
         }
 
         /**
+         * CaseCodes.
+         */
+        public Builder caseCodes(java.util.List<String> caseCodes) {
+            String caseCodesShrink = shrink(caseCodes, "CaseCodes", "json");
+            this.putQueryParameter("CaseCodes", caseCodesShrink);
+            this.caseCodes = caseCodes;
+            return this;
+        }
+
+        /**
          * Current.
          */
         public Builder current(String current) {
@@ -203,6 +279,16 @@ public class QueryAccountSafetyIncidentRequest extends Request {
         public Builder eventId(String eventId) {
             this.putQueryParameter("EventId", eventId);
             this.eventId = eventId;
+            return this;
+        }
+
+        /**
+         * EventIds.
+         */
+        public Builder eventIds(java.util.List<String> eventIds) {
+            String eventIdsShrink = shrink(eventIds, "EventIds", "json");
+            this.putQueryParameter("EventIds", eventIdsShrink);
+            this.eventIds = eventIds;
             return this;
         }
 
@@ -248,6 +334,16 @@ public class QueryAccountSafetyIncidentRequest extends Request {
         public Builder status(String status) {
             this.putQueryParameter("Status", status);
             this.status = status;
+            return this;
+        }
+
+        /**
+         * Statuses.
+         */
+        public Builder statuses(java.util.List<String> statuses) {
+            String statusesShrink = shrink(statuses, "Statuses", "json");
+            this.putQueryParameter("Statuses", statusesShrink);
+            this.statuses = statuses;
             return this;
         }
 
